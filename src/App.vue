c9f61569ebd2162159d63b746832a03cf456ac9e
carlosmg96 <rguezcarlosmg96@outlook.com>
2024-01-12 23:37:38 -0600

    intento de layaut

---
+++ src/App.vue
@@ -0,0 +1,102 @@
+<script setup>
+import { ref } from 'vue';
+</script>
+
+<template>
+  <div id="app">
+    <!-- Aquí está el Navbar -->
+    <div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+
+    <b-navbar-toggle target="sidebar"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template #button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+
+    <!-- Zona del contenido, content -->
+    <b-container fluid>
+      <b-row>
+        <b-col md="3">
+        <div style="margin-top: 15px;">
+            <b-sidebar class="mt-5" id="sidebar" title="Sidebar" visible>
+          <template #default="{ hide }">
+        <div class="p-3">
+          <h4 id="sidebar-no-header-title">Custom header sidebar</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+            in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+          </p>
+          <nav class="mb-3">
+            <b-nav vertical>
+              <b-nav-item active @click="hide">Active</b-nav-item>
+              <b-nav-item href="#link-1" @click="hide">Link</b-nav-item>
+              <b-nav-item href="#link-2" @click="hide">Another Link</b-nav-item>
+            </b-nav>
+          </nav>
+          <b-button variant="primary" block @click="hide">Close Sidebar</b-button>
+        </div>
+      </template>   
+          </b-sidebar>
+        </div>
+        </b-col>
+
+        <b-col md="9">
+         <div class="text-center">
+          <h1>Button Group</h1>
+         <h4>Group a series of buttons together on a single line with</h4>
+         <div class="mt-5">
+         <h3>Overview</h3>
+         <p>Button Groups are an easy way to group a series of buttons together</p>
+         <div>
+  <div>
+    <b-button-group>
+      <b-button>Button 1</b-button>
+      <b-button>Button 2</b-button>
+      <b-button>Button 3</b-button>
+    </b-button-group>
+  </div>
+  <div class="mt-3">
+    <b-button-group>
+      <b-button variant="success">Success</b-button>
+      <b-button variant="info">Info</b-button>
+      <b-button variant="warning">Warning</b-button>
+    </b-button-group>
+  </div>
+</div>
+         </div>
+         </div>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<style scoped>
+</style>
